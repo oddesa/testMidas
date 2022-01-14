a17da06d96783979e55b14af8dcfc65269c49a3b
oddesa <jehnsen.hirena@sci.ui.ac.id>
2022-01-14 09:23:54 +0700

    first task

---
+++ midasJayaTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  midasJayaTest
+//
+//  Created by Jehnsen Hirena Kane on 14/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
